5d93b9b378d540a6404790145af8226ad6e5c310
Shawky Abdalrafea <sabdalrafea@ntgclarity.com>
2025-10-21 17:18:33 +0300

    solved the AuthenticationManagerBuilder

--- TimeSheet/src/main/java/com/timesheet/config/SecurityConfig.java
+++ TimeSheet/src/main/java/com/timesheet/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -54,9 +55,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) {
-//    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
